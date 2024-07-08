f5c6c6a132bb43d68598734b80f648309f271527
c-karaolis <kostaskaraolis@gmail.com>
2024-07-08 19:29:25 +0300

    leafnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_repr(self):
@@ -21,3 +21,12 @@ class TestHTMLNode(unittest.TestCase):
         
     def test_props_to_html_with_multiple_props(self):
         node = HTMLNode("div", "This is a text node", props={"class": "bold", "href": "http://www.google.com"})
+        
+    # LeafNode tests
+    def test_to_html_no_children(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_to_html_no_tag(self):
+        node = LeafNode(None, "Hello, world!")
+        self.assertEqual(node.to_html(), "Hello, world!")
