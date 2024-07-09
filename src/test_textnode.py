ff5c4982d9a36a13bbc199986cafb6713bd0be96
c-karaolis <kostaskaraolis@gmail.com>
2024-07-09 18:22:58 +0300

    text node to html node and tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,28 +1,43 @@
 import unittest
 
-from textnode import TextNode
+from textnode import (
+    TextNode,
+    text_type_text,
+    text_type_bold,
+    text_type_italic,
+    text_type_code,
+    text_type_image,
+    text_type_link,
+)
 
 
 class TestTextNode(unittest.TestCase):
-    def test_equal(self):
-        node = TextNode("This is a text node", "bold")
-        node2 = TextNode("This is a text node", "bold")
+    def test_eq(self):
+        node = TextNode("This is a text node", text_type_text)
+        node2 = TextNode("This is a text node", text_type_text)
         self.assertEqual(node, node2)
-    
-    def test_not_equal(self):
-        node = TextNode("This is a sdasd node", "bold")
-        node2 = TextNode("This is a text node", "bold")
-        self.assertNotEqual(node, node2)
-    
-    def test_repr(self):
-        node = TextNode("This is a text node", "bold")
-        self.assertEqual(repr(node), "TextNode(This is a text node,bold,None)")
-        
-    def test_repr_with_url(self):
-        node = TextNode("This is a text node", "bold", "http://www.google.com")
-        self.assertEqual(repr(node), "TextNode(This is a text node,bold,http://www.google.com)")
 
-    
+    def test_eq_false(self):
+        node = TextNode("This is a text node", text_type_text)
+        node2 = TextNode("This is a text node", text_type_bold)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_false2(self):
+        node = TextNode("This is a text node", text_type_text)
+        node2 = TextNode("This is a text node2", text_type_text)
+        self.assertNotEqual(node, node2)
+
+    def test_eq_url(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        node2 = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        self.assertEqual(node, node2)
+
+    def test_repr(self):
+        node = TextNode("This is a text node", text_type_text, "https://www.boot.dev")
+        self.assertEqual(
+            "TextNode(This is a text node, text, https://www.boot.dev)", repr(node)
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
